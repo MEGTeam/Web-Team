aa8552de743c4371a432a1189e2bffd219f67929
Belal-2000 <belalma3arek#@gmail.com>
2022-02-26 02:44:30 +0200

    Background change effect

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// to change background img every 2.5 sec
+
+let backgroundNum = 0;
+
+window.onload = function () {
+    var backgroundImg=["../images/home-bg-slideshow1.jpg",
+                      "../images/home-bg-slideshow2.jpg"
+                      ]
+    
+      setInterval(changeImage, 2500);
+    function changeImage() {
+        const target = document.querySelector(".titleback")
+
+        backgroundNum = backgroundNum == 0 ? 1 : 0;
+
+        target.style.backgroundImage = "url('"+backgroundImg[backgroundNum]+"')";
+    }
+  }
