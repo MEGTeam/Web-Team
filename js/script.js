da0bad152d5fc5806340785b5001be6b8492a86c
Belal-2000 <belalma3arek#@gmail.com>
2022-02-28 04:16:40 +0200

    Finished task

--- js/script.js
+++ js/script.js
@@ -11,4 +11,24 @@ window.onload = function () {
         changeImageInt = changeImageInt === 0 ? 1 : 0
         target.style.backgroundImage = "url('" + backgroundImgs[changeImageInt] + "')";
     }
-  }
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+document.addEventListener("scroll", () => {
+    const viewedElement = document.querySelector(".small_nav")
+    const target = document.querySelector(".grid_containor")
+    if (isInViewport(viewedElement)) {
+        target.style.opacity = 1        
+    }
+})
+
