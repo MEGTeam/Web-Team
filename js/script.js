a57fd07a1e50bc4effc212b2faab19600d0ffef6
Belal-2000 <belalma3arek#@gmail.com>
2022-03-01 01:45:59 +0200

    Make js more simple

--- js/script.js
+++ js/script.js
@@ -15,19 +15,13 @@ window.onload = function () {
 
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    return rect.top <= 600 ;
 }
 
 
 document.addEventListener("scroll", () => {
-    const viewedElement = document.querySelector(".small_nav")
     const target = document.querySelector(".grid_containor")
-    if (isInViewport(viewedElement)) {
+    if (isInViewport(target)) {
         target.style.opacity = 1        
     }
 })
